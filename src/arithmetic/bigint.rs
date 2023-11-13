37c4f5c100dd6c4e944e5d3289f1f5ff14610af3
Brian Smith <brian@briansmith.org>
2023-11-13 13:56:05 -0800

    bigint: NFC: Move One to its own submodule.

--- src/arithmetic/bigint.rs
+++ src/arithmetic/bigint.rs
@@ -39,6 +39,7 @@
 use self::boxed_limbs::BoxedLimbs;
 pub(crate) use self::{
     modulus::{Modulus, OwnedModulusWithOne, MODULUS_MAX_LIMBS},
+    one::One,
     private_exponent::PrivateExponent,
 };
 use super::n0::N0;
@@ -47,14 +48,14 @@ use crate::{
     arithmetic::montgomery::*,
     bits::BitLength,
     c, cpu, error,
-    limb::{self, Limb, LimbMask, LIMB_BITS},
-    polyfill::u64_from_usize,
+    limb::{self, Limb, LimbMask},
 };
 use alloc::vec;
 use core::{marker::PhantomData, num::NonZeroU64};
 
 mod boxed_limbs;
 mod modulus;
+mod one;
 mod private_exponent;
 
 /// A modulus *s* that is smaller than another modulus *l* so every element of
@@ -273,79 +274,6 @@ pub fn elem_sub<M, E>(mut a: Elem<M, E>, b: &Elem<M, E>, m: &Modulus<M>) -> Elem
     a
 }
 
-// The value 1, Montgomery-encoded some number of times.
-pub struct One<M, E>(Elem<M, E>);
-
-impl<M> One<M, RR> {
-    // Returns RR = = R**2 (mod n) where R = 2**r is the smallest power of
-    // 2**LIMB_BITS such that R > m.
-    //
-    // Even though the assembly on some 32-bit platforms works with 64-bit
-    // values, using `LIMB_BITS` here, rather than `N0::LIMBS_USED * LIMB_BITS`,
-    // is correct because R**2 will still be a multiple of the latter as
-    // `N0::LIMBS_USED` is either one or two.
-    fn newRR(m: &Modulus<M>) -> Self {
-        let m_bits = m.len_bits().as_usize_bits();
-        let r = (m_bits + (LIMB_BITS - 1)) / LIMB_BITS * LIMB_BITS;
-
-        // base = 2**(lg m - 1).
-        let bit = m_bits - 1;
-        let mut base = m.zero();
-        base.limbs[bit / LIMB_BITS] = 1 << (bit % LIMB_BITS);
-
-        // Double `base` so that base == R == 2**r (mod m). For normal moduli
-        // that have the high bit of the highest limb set, this requires one
-        // doubling. Unusual moduli require more doublings but we are less
-        // concerned about the performance of those.
-        //
-        // Then double `base` again so that base == 2*R (mod n), i.e. `2` in
-        // Montgomery form (`elem_exp_vartime()` requires the base to be in
-        // Montgomery form). Then compute
-        // RR = R**2 == base**r == R**r == (2**r)**r (mod n).
-        //
-        // Take advantage of the fact that `elem_mul_by_2` is faster than
-        // `elem_squared` by replacing some of the early squarings with shifts.
-        // TODO: Benchmark shift vs. squaring performance to determine the
-        // optimal value of `LG_BASE`.
-        const LG_BASE: usize = 2; // Shifts vs. squaring trade-off.
-        debug_assert_eq!(LG_BASE.count_ones(), 1); // Must be 2**n for n >= 0.
-        let shifts = r - bit + LG_BASE;
-        // `m_bits >= LG_BASE` (for the currently chosen value of `LG_BASE`)
-        // since we require the modulus to have at least `MODULUS_MIN_LIMBS`
-        // limbs. `r >= m_bits` as seen above. So `r >= LG_BASE` and thus
-        // `r / LG_BASE` is non-zero.
-        //
-        // The maximum value of `r` is determined by
-        // `MODULUS_MAX_LIMBS * LIMB_BITS`. Further `r` is a multiple of
-        // `LIMB_BITS` so the maximum Hamming Weight is bounded by
-        // `MODULUS_MAX_LIMBS`. For the common case of {2048, 4096, 8192}-bit
-        // moduli the Hamming weight is 1. For the other common case of 3072
-        // the Hamming weight is 2.
-        let exponent = NonZeroU64::new(u64_from_usize(r / LG_BASE)).unwrap();
-        for _ in 0..shifts {
-            elem_mul_by_2(&mut base, m)
-        }
-        let RR = elem_exp_vartime(base, exponent, m);
-
-        Self(Elem {
-            limbs: RR.limbs,
-            encoding: PhantomData, // PhantomData<RR>
-        })
-    }
-}
-
-impl<M, E> AsRef<Elem<M, E>> for One<M, E> {
-    fn as_ref(&self) -> &Elem<M, E> {
-        &self.0
-    }
-}
-
-impl<M: PublicModulus, E> Clone for One<M, E> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
-    }
-}
-
 /// Calculates base**exponent (mod m).
 ///
 /// The run time  is a function of the number of limbs in `m` and the bit
@@ -452,7 +380,13 @@ pub fn elem_exp_consttime<M>(
         // `table` was initialized to zero and hasn't changed.
         debug_assert!(acc.iter().all(|&value| value == 0));
         acc[0] = 1;
-        limbs_mont_mul(acc, &oneRR.0.limbs, m.limbs(), m.n0(), m.cpu_features());
+        limbs_mont_mul(
+            acc,
+            &oneRR.as_ref().limbs,
+            m.limbs(),
+            m.n0(),
+            m.cpu_features(),
+        );
     }
 
     entry_mut(&mut table, 1, num_limbs).copy_from_slice(&base.limbs);
@@ -651,7 +585,7 @@ pub fn elem_exp_consttime<M>(
     // encode it.
     debug_assert!(acc.iter().all(|&value| value == 0));
     acc[0] = 1;
-    limbs_mont_mul(acc, &oneRR.0.limbs, m_cached, n0, cpu_features);
+    limbs_mont_mul(acc, &oneRR.as_ref().limbs, m_cached, n0, cpu_features);
     scatter(table, acc, 0, num_limbs);
 
     // acc = base**1 (i.e. base).
